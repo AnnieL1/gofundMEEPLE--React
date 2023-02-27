6653374e3f9d0a5c31ff0fc395a4bea9c3568933
AnnieL1 <annieliang17@gmail.com>
2023-02-28 02:16:21 +1100

    28 feb commit

--- src/pages/LogoutPage.jsx
+++ src/pages/LogoutPage.jsx
@@ -4,7 +4,7 @@ import LogoutForm from '../components/LogoutForm';
 const LogoutPage = () => {
     const token = window.localStorage.getItem("token")
     // console.log( token !== null)
-    return token !== undefined && token !== null ? <LogoutForm /> :    
+    return token == undefined && token == null ? <LogoutForm /> :    
         <h1>You are already logged out! </h1>
 };
 
